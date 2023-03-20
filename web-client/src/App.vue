cfe085df6a6d2f2b898186b427c2859c341e0d76
Thomas Ferro <t.ferro184@gmail.com>
2023-03-20 09:07:08 +0100

    chore: front setup

---
+++ web-client/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-card class="h-100">
+    <v-layout>
+      <v-app-bar title="L'édition libre">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </v-app-bar>
+      <v-main style="min-height: 300px;">
+        <RouterView />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
